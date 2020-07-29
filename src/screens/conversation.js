1a5225e50a43ef8c99d74781609b761aae99bf52
muhammetdemirci <mddemircii5@gmail.com>
2020-07-29 09:37:17 +0300

    add IconButton to navigation bar on conversationScreen

--- src/screens/conversation.js
+++ src/screens/conversation.js
@@ -3,19 +3,21 @@ import { View, Text } from 'react-native'
 import { IconButton, UserCard } from '../components';
 
 class ConversationScreen extends React.Component {
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: "Chat App",
+            headerLeft: () => (<IconButton
+                onPress={() => navigation.navigate('Userlist')}
+                name={"add"} size={36} iconStyle={{ color: 'blue' }} />)
+        };
+    };
 
     render() {
         return (<View style={{
             flex: 1,
-            justifyContent: 'center', alignItems: 'center',
             backgroundColor: '#f4f4f4'
 
         }} >
-            <Text>ConversationScreen # TODO</Text>
-            <IconButton
-                onPress={() => this.props.navigation.navigate('Userlist')}
-                name={"add"} size={50} iconStyle={{ color: 'blue' }} />
-
             <UserCard onPress={(username) => this.props.navigation.navigate('Chat', { username })}
                 username={'Ali'} />
             <UserCard onPress={(username) => this.props.navigation.navigate('Chat', { username })}
