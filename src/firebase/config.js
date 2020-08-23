fc2a53b96e5521c07d842446f1cbee3b07b791c1
Muhammet Demirci <mddemircii5@gmail.com>
2020-08-23 09:43:49 +0300

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const config = {
+    apiKey: "AIzaSyDUBR0QcepxVQ05UFDoLn8PS2lqqC4yvW8",
+    authDomain: "chatapp-24922.firebaseapp.com",
+    databaseURL: "https://chatapp-24922.firebaseio.com/",
+    projectId: "chatapp-24922",
+    storageBucket: "chatapp-24922.appspot.com",
+    messagingSenderId: "909145383199",
+    appId: "1:909145383199:web:8ed27ede42882589a6902a"
+};
